faae7dbc4916befbec0e5e05defbc1122ef83638
ray <ray@raydeMacBook-Pro.local>
2026-07-11 15:30:19 +0800

    GPS：授权调用移到applicationDidBecomeActive

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -9,8 +9,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     let locationManager = CLLocationManager()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        locationManager.requestTemporaryFullAccuracyAuthorization(withPurposeKey: "LocationAccuracy")
         return true
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        locationManager.requestTemporaryFullAccuracyAuthorization(withPurposeKey: "LocationAccuracy")
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
@@ -25,10 +28,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
