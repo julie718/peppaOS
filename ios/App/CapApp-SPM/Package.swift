2b48f2cbc69a1f01751699d1df707ed20b5101d1
ray <29263412@qq.com>
2026-07-21 20:19:08 +0800

    添加 HealthKit 插件: @krzysztofkostecki/capacitor-health (iOS Health 数据读写)

--- ios/App/CapApp-SPM/Package.swift
+++ ios/App/CapApp-SPM/Package.swift
@@ -12,7 +12,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.3.1"),
-        .package(name: "CapacitorGeolocation", path: "../../../node_modules/@capacitor/geolocation")
+        .package(name: "CapacitorGeolocation", path: "../../../node_modules/@capacitor/geolocation"),
+        .package(name: "KrzysztofkosteckiCapacitorHealth", path: "../../../node_modules/@krzysztofkostecki/capacitor-health")
     ],
     targets: [
         .target(
@@ -20,7 +21,8 @@ let package = Package(
             dependencies: [
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
                 .product(name: "Cordova", package: "capacitor-swift-pm"),
-                .product(name: "CapacitorGeolocation", package: "CapacitorGeolocation")
+                .product(name: "CapacitorGeolocation", package: "CapacitorGeolocation"),
+                .product(name: "KrzysztofkosteckiCapacitorHealth", package: "KrzysztofkosteckiCapacitorHealth")
             ]
         )
     ]
